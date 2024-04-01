ccc04b1d8570e6f3d99fccceea0ab7441455033a
Max Belousov <macss.belousov699@gmail.com>
2024-04-01 22:59:49 +0300

    new build

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {AppComponent} from "./app.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  exports: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers:[],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
