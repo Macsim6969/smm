0fe3537e52618fd54714eaf90b56b10edd9c843a
Max Belousov <macss.belousov699@gmail.com>
2024-04-24 21:10:09 +0300

    updated register form

--- src/app/modules/auth/@shared/components/register/register.component.ts
+++ src/app/modules/auth/@shared/components/register/register.component.ts
@@ -1,34 +1,35 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../../services/auth.service";
-import {Router} from "@angular/router";
-import {Subscription} from "rxjs";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent implements OnInit, OnDestroy{
+export class RegisterComponent implements OnInit, OnDestroy {
   public form: FormGroup;
-private authSubscription: Subscription;
+  private authSubscription: Subscription;
   constructor(
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) { }
   ngOnInit() {
     this.form = new FormGroup<any>({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      doublePassword: new FormControl('', [Validators.required, Validators.minLength(8)])
     })
   }
 
-  public submit(){
-    const formData = {...this.form.value}
+  public submit() {
+    const formData = { ...this.form.value }
     this.form.reset()
 
-   this.authSubscription = this.authService.sigUp(formData).subscribe((data) =>{
+    this.authSubscription = this.authService.sigUp(formData).subscribe((data) => {
       data ? this.router.navigate(['auth/login']).then() : null;
     })
   }
